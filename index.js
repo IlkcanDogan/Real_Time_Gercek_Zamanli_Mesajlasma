dd6e7d7429bdf90a22a2580e9c5476bbcc98158d
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-04-28 03:46:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ io.on('connection', (socket) => {
 
 });
 
-var port = process.env.PORT || 3001;
+var port = 3001;
 
 http.listen(port, function () {
   //console.log('http://localhost:' + port + " adresi dinleniyor...");
